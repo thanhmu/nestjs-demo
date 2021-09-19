2efe9ecf0c9304be854aaa5135788f84cf5b966a
= <thanhnt@antsomi.com>
2021-09-19 23:26:39 +0700

    Add code auth part-01

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,27 @@
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { AuthService } from './auth.service';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('/signup')
+  signUp(@Body() authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    return this.authService.signUp(authCredentialsDto);
+  }
+
+  @Post('/signin')
+  signIn(
+    @Body() authCredentialsDto: AuthCredentialsDto,
+  ): Promise<{ accessToken: string }> {
+    return this.authService.signIn(authCredentialsDto);
+  }
+
+  @Post('/test')
+  @UseGuards(AuthGuard())
+  test(@Req() req) {
+    console.log(req);
+  }
+}
---
+++ src/auth/user.repository.ts
@@ -0,0 +1,31 @@
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { EntityRepository, Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { User } from './user.entity';
+import * as bcrypt from 'bcrypt';
+
+@EntityRepository(User)
+export class UserRepository extends Repository<User> {
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+
+    // has password
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = this.create({ username, password: hashedPassword });
+
+    try {
+      await this.save(user);
+    } catch (e) {
+      console.log(e);
+      if (e.code === '23505') {
+        throw new ConflictException('Username already exits');
+      }
+      throw new InternalServerErrorException();
+    }
+  }
+}
