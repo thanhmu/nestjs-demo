ed2dedfa68f3db0d5dad3a9cc65e682d1eb245cc
= <thanhnt@antsomi.com>
2021-09-25 15:06:35 +0700

    Going live

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,7 +15,13 @@ import { configValidationSchema } from './config.schema';
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) => {
+        const isProduction = configService.get('STAGE') === 'prod';
+
         return {
+          ssl: isProduction,
+          extra: {
+            ssl: isProduction ? { rejectUnauthorized: false } : null,
+          },
           type: 'postgres',
           autoLoadEntities: true,
           synchronize: true,
--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const logger = new Logger();
-  const port = 3000;
+  const port = process.env.PORT;
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
