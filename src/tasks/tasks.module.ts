56797cd7e78a15e1fc8575fc3281c3c4a80dc437
= <thanhnt@antsomi.com>
2021-09-25 11:35:56 +0700

    Add Configuration

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from 'src/auth/auth.module';
 import { TasksController } from './tasks.controller';
